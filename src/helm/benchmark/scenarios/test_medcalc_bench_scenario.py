becccb734562cb941e5aee0cf8fba6d8f89599bb
Iulian Igas <iulian@pacific.ai>
2026-05-05 14:37:25 +0300

    Fixed failing tests with removing deprecated versions

--- src/helm/benchmark/scenarios/test_medcalc_bench_scenario.py
+++ src/helm/benchmark/scenarios/test_medcalc_bench_scenario.py
@@ -13,27 +13,6 @@ def test_medcalc_bench_scenario():
 
         assert instances[0].split == "test"
 
-
-@pytest.mark.scenarios
-def test_medcalc_bench_v1_0_scenario():
-    with TemporaryDirectory() as tmpdir:
-        # Test for the MedCalc-Bench scenario
-        scenario = MedCalcBenchScenario()
-        instances = scenario.get_instances(tmpdir, "v1.0")
-
-        assert instances[0].split == "test"
-
-
-@pytest.mark.scenarios
-def test_medcalc_bench_v1_1_scenario():
-    with TemporaryDirectory() as tmpdir:
-        # Test for the MedCalc-Bench scenario
-        scenario = MedCalcBenchScenario()
-        instances = scenario.get_instances(tmpdir, "v1.1")
-
-        assert instances[0].split == "test"
-
-
 @pytest.mark.scenarios
 def test_medcalc_bench_v1_2_scenario():
     with TemporaryDirectory() as tmpdir:
